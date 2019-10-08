b4a566c0242d16b8c5f3c3a9c0eaf333ec3a10f9
arw <atrigueiro@yahoo.com>
2019-10-08 11:13:43 -0700

    Write2MSMQ
    Drop a message on a MSMQ message queeu

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Messaging;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WriteToMSMQClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageQueue msgQ = new MessageQueue(txtQueuePath.Text);
+                msgQ.Formatter = new XmlMessageFormatter(new Type[]{typeof(XmlDocument)});
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(txtMessage.Text);
+                Message m1 = new Message();
+                m1.Body = xml;
+                msgQ.Send(m1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error sending to queue");                
+            }
+        }
+    }
+}
